08ea20f17e47a2561df2c9353685d497c5763adb
JooYoung Jang <joojang2019@u.northwestern.edu>
2020-01-26 17:14:01 -0600

    added PlayButton.js

--- lofi-frontend/src/components/PlayButton.js
+++ lofi-frontend/src/components/PlayButton.js
@@ -1,10 +1,4 @@
 import React, { useState } from "react";
-import IconButton from "@material-ui/core/IconButton";
-import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
-import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
-import { grey } from '@material-ui/core/colors';
-
-const PlayButtonStyle = {fontSize: 100, color: grey[50]}
 
 const PlayButton = ({
   instrument,
@@ -62,10 +56,9 @@ const PlayButton = ({
   };
 
   return (
-    <IconButton onClick={handleClick}>
-      {(playState ? <PlayCircleOutlineIcon style={PlayButtonStyle} />
-                  : <PauseCircleOutlineIcon style={PlayButtonStyle} />)}
-    </IconButton>
+    <button onClick={handleClick}>
+      {instrument + (playState ? " play" : " pause")}
+    </button>
   );
 };
 
