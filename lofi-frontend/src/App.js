4600786da5d124d2cfc43a94cdc0a3b23eccc342
JooYoung Jang <joojang2019@u.northwestern.edu>
2020-01-26 17:05:19 -0600

    RNN started

--- lofi-frontend/src/App.js
+++ lofi-frontend/src/App.js
@@ -2,62 +2,86 @@ import React from "react";
 import "./App.css";
 import PlayButton from "./components/PlayButton";
 import * as mm from "@magenta/music";
+import * as Tone from "tone";
+import * as Tonal from "tonal";
 
 function App() {
   const player = new mm.SoundFontPlayer(
     "https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus"
   );
 
-  const drums = {
-    notes: [
-      { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
-      { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
-      { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-      { pitch: 50, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-      { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 38, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 36, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-      { pitch: 42, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-      { pitch: 46, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-      { pitch: 42, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-      { pitch: 48, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-      { pitch: 50, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true }
+  const rnnPlayer = new mm.SoundFontPlayer(
+    "https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus"
+  );
+
+  const improvRNN = new mm.MusicRNN(
+    "https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv"
+  );
+  improvRNN.initialize();
+
+  const synth = new Tone.Synth().toMaster();
+  const { midi, Note } = Tonal;
+
+  const sequence = {
+    ticksPerQuarter: 220,
+    totalTime: 28.5,
+    timeSignatures: [
+      {
+        time: 0,
+        numerator: 4,
+        denominator: 4
+      }
     ],
-    quantizationInfo: { stepsPerQuarter: 4 },
-    tempos: [{ time: 0, qpm: 120 }],
-    totalQuantizedSteps: 11
+    tempos: [
+      {
+        time: 0,
+        qpm: 120
+      }
+    ],
+    notes: [
+      { pitch: midi("Gb4"), startTime: 0, endTime: 1 },
+      { pitch: midi("F4"), startTime: 1, endTime: 3.5 },
+      { pitch: midi("Ab4"), startTime: 3.5, endTime: 4 },
+      { pitch: midi("C5"), startTime: 4, endTime: 4.5 },
+      { pitch: midi("Eb5"), startTime: 4.5, endTime: 5 },
+      { pitch: midi("Gb5"), startTime: 5, endTime: 6 },
+      { pitch: midi("F5"), startTime: 6, endTime: 7 },
+      { pitch: midi("E5"), startTime: 7, endTime: 8 },
+      { pitch: midi("Eb5"), startTime: 8, endTime: 8.5 },
+      { pitch: midi("C5"), startTime: 8.5, endTime: 9 },
+      { pitch: midi("G4"), startTime: 9, endTime: 11.5 },
+      { pitch: midi("F4"), startTime: 11.5, endTime: 12 },
+      { pitch: midi("Ab4"), startTime: 12, endTime: 12.5 },
+      { pitch: midi("C5"), startTime: 12.5, endTime: 13 },
+      { pitch: midi("Eb5"), startTime: 13, endTime: 14 },
+      { pitch: midi("D5"), startTime: 14, endTime: 15 },
+      { pitch: midi("Db5"), startTime: 15, endTime: 16 },
+      { pitch: midi("C5"), startTime: 16, endTime: 16.5 },
+      { pitch: midi("F5"), startTime: 16.5, endTime: 17 },
+      { pitch: midi("F4"), startTime: 17, endTime: 19.5 },
+      { pitch: midi("G4"), startTime: 19.5, endTime: 20 },
+      { pitch: midi("Ab4"), startTime: 20, endTime: 20.5 },
+      { pitch: midi("C5"), startTime: 20.5, endTime: 21 },
+      { pitch: midi("Eb5"), startTime: 21, endTime: 21.5 },
+      { pitch: midi("C5"), startTime: 21.5, endTime: 22 },
+      { pitch: midi("Eb5"), startTime: 22, endTime: 22.5 },
+      { pitch: midi("C5"), startTime: 22.5, endTime: 24.5 },
+      { pitch: midi("Eb5"), startTime: 24.5, endTime: 25.5 },
+      { pitch: midi("G4"), startTime: 25.5, endTime: 28.5 }
+    ]
   };
 
-  const piano = {
-    notes: [
-      { pitch: 60, startTime: 0.0, endTime: 0.5 },
-      { pitch: 60, startTime: 0.5, endTime: 1.0 },
-      { pitch: 67, startTime: 1.0, endTime: 1.5 },
-      { pitch: 67, startTime: 1.5, endTime: 2.0 },
-      { pitch: 69, startTime: 2.0, endTime: 2.5 },
-      { pitch: 69, startTime: 2.5, endTime: 3.0 },
-      { pitch: 67, startTime: 3.0, endTime: 4.0 },
-      { pitch: 65, startTime: 4.0, endTime: 4.5 },
-      { pitch: 65, startTime: 4.5, endTime: 5.0 },
-      { pitch: 64, startTime: 5.0, endTime: 5.5 },
-      { pitch: 64, startTime: 5.5, endTime: 6.0 },
-      { pitch: 62, startTime: 6.0, endTime: 6.5 },
-      { pitch: 62, startTime: 6.5, endTime: 7.0 },
-      { pitch: 60, startTime: 7.0, endTime: 8.0 }
-    ],
-    totalTime: 8
-  };
+  const quantizedSequence = mm.sequences.quantizeNoteSequence(sequence, 1);
 
   return (
     <div>
-      <PlayButton instrument="drums" player={player} drums={drums} />
-      <PlayButton instrument="piano" player={player} piano={piano} />
+      <PlayButton
+        instrument="rnn"
+        synth={synth}
+        improvRNN={improvRNN}
+        quantizedSequence={quantizedSequence}
+        Note={Note}
+      />
     </div>
   );
 }
--- lofi-frontend/src/components/PlayButton.js
+++ lofi-frontend/src/components/PlayButton.js
@@ -1,13 +1,59 @@
 import React, { useState } from "react";
 
-const PlayButton = ({ instrument, player, drums, piano }) => {
-  const [playState, setPlayState] = useState(true);
+const PlayButton = ({
+  instrument,
+
+  improvRNN,
+  synth,
+  quantizedSequence,
+  Note
+}) => {
+  const [playState, setPlayState] = useState(false);
 
   const handleClick = () => {
     setPlayState(!playState);
-    if (playState === true && instrument === "drums") player.start(drums);
-    if (playState === true && instrument === "piano") player.start(piano);
-    if (playState === false) player.stop();
+    if (playState === true && improvRNN) startProgram(improvRNN);
+  };
+
+  const startProgram = async improvRNN => {
+    try {
+      await improvRNN.initialize();
+      let improvisedMelody = await improvRNN.continueSequence(
+        quantizedSequence,
+        60,
+        2.0,
+        [
+          "Bm",
+          "Bbm",
+          "Gb7",
+          "F7",
+          "Ab",
+          "Ab7",
+          "G7",
+          "Gb7",
+          "F7",
+          "Bb7",
+          "Eb7",
+          "AM7"
+        ]
+      );
+
+      const playGeneratedMelody = () => {
+        console.log(1);
+        improvisedMelody.notes.forEach(note => {
+          synth.triggerAttackRelease(
+            Note.fromMidi(note.pitch),
+            note.quantizedEndStep - note.quantizedStartStep,
+            note.quantizedStartStep
+          );
+        });
+        return true;
+      };
+
+      playGeneratedMelody();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
